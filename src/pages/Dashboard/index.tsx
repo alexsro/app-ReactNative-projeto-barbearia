799fae5e31f2a29a5e6266fa2527bae3f608f5f3
Alex Ortelan <alex000sander@gmail.com>
2021-10-04 21:27:02 -0300

    Creation off pages structure and begin of dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../RootPagesList';
 
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
+type dashboardScreenProps = NativeStackNavigationProp<
+  RootStackParamList,
+  'Profile'
+>;
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation<dashboardScreenProps>();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar
+            source={{
+              uri: 'https://gestor.fiorilli.com.br/administrador.dll/cache/administrador_exe/n0/res/__177E96801EBBC96337DE776A.png',
+            }}
+          />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
--- src/pages/RootPagesList.ts
+++ src/pages/RootPagesList.ts
@@ -2,4 +2,7 @@ export type RootStackParamList = {
   SignIn: undefined;
   SignUp: undefined;
   Dashboard: undefined;
+  Profile: undefined;
+  AppointmentCreated: undefined;
+  CreateAppointment: undefined;
 };
